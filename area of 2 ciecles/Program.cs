6683ba3501815aed76a17ac3668dd7cfd944e70a
ST10215069_Nkosinathi Ngozo <104794600+ST10215069-Nkosinathi-Ngozo@users.noreply.github.com>
2023-04-28 01:12:21 +0200

    Add files via upload

---
+++ area of 2 ciecles/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace area_of_2_ciecles
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            area a = new area(10);
+            Console.WriteLine(a.calculateAreaOfBigCircle());
+        }
+    }
+}
---
+++ area of 2 ciecles/area.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace area_of_2_ciecles
+{
+    internal class area
+    {
+        
+        double radiusSmall;
+        double pi = 3.14;
+
+        public area(double radiusSmall) { 
+            this.radiusSmall = radiusSmall;
+        
+        }
+        public double calculateAreaOfSmallCircle() {
+            double area = pi * radiusSmall * radiusSmall;
+            return area;
+        }
+        public double calculateAreaOfBigCircle()
+        {
+            double area = calculateAreaOfSmallCircle() * 2;
+            return area;
+        }
+
+
+    }
+}
